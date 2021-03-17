407eaaa05b3a20e995d15d9820c239af84526607
snehasamuel <79563674+snehasamuel@users.noreply.github.com>
2021-03-17 10:18:43 +0530

    Add files via upload

---
+++ TOP.C
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,k,a[10][10],indeg[10],flag[10],count=0;
+clrscr();
+printf("enter the number of vertices :\n");
+scanf("%d",&n);
+printf("Enter the adjacency matrix: \n");
+for(i=0;i<n;i++)
+{
+printf("Enter the row %d\n",i+1);
+for(j=0;j<n;j++)
+scanf("%d",&a[i][j]);
+}
+for(i=0;i<n;i++)
+{
+indeg[i]=0;
+flag[i]=0;
+
+}
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+{
+indeg[i]=indeg[i]+a[i][j];
+}
+printf("In the topographical order :");
+while(count<n)
+{
+for(k=0;k<n;k++)
+{
+if((indeg[k]==0)&&(flag[k]==0))
+{
+printf("%d",(k+1));
+flag[k]=1;
+}
+for(i=0;i<n;i++)
+{
+if(a[i][j]==1)
+indeg[k]--;
+}
+}
+count++;
+}
+getch();
+return 0;
+
+}
+
